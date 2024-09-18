3dc1c3a76c8c172ec2041c2f9c80c81c389b6078
akhilsajigit <akhilsaji5470@gmail.com>
2024-09-18 13:39:50 +0530

    Updated user messaging

--- assets/js/main.js
+++ assets/js/main.js
@@ -188,3 +188,21 @@ downloadButton.addEventListener('click', (e) => {
   link.click();
 });
 
+/*==================== Script to get User Messages and Success ====================*/ 
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwIeOujIIM7-_5Kpa8fp9NeF44RJ6Jiq14eoeIwwAbCCWJKIyTlFGVK4-NlpQPXD4Pa4g/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message Sent Successfully <i class='uil uil-envelope-heart'></i>"
+        setTimeout(function(){
+          msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
